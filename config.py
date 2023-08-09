30cd2edc924fe5a29b298587dc89047134928345
nurfachridaffa17 <nurfachridaffa17@gmail.com>
2023-08-09 03:57:29 +0000

    config.py

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = 'super secret key'
     URL_ENDPOINT = "http://192.168.88.79:8098/api/v2/transaction/list"
-    HEADERS_ENDPOINT = "SESSION=ODk3MzBjMzItZDEwMy00ODAxLTliZDgtZWZiYjMyMTU4ZDQ5"
+    HEADERS_ENDPOINT = "SESSION=MjlhMDc3YzktMTE1OS00Y2VhLTk0ZjYtZDRjOWI3YTdlNTU0"
